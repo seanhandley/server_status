0095f443b1e5089933f8762d8f42f88ce9d9cb1a
Sean Handley <sean.handley@gmail.com>
2012-01-30 08:19:01 +0000

    Added controller specs.

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,9 +1,44 @@
 require 'spec_helper'
 
 describe EventsController do
-  context '#index' do
+
+  context ' GET index' do
     it 'should load successfully' do
       get :index
+      response.should be_success
+    end
+
+    it "should set up active events" do
+      Event.stub(:active).and_return([mock_model(Event, {})])
+      get :index
+      assigns(:active_events).count.should == 1
+    end
+
+    it "should set up scheduled events" do
+      Event.stub(:scheduled).and_return([mock_model(Event, {})])
+      get :index
+      assigns(:scheduled_events).count.should == 1
+    end
+
+    it "should set up resolved events" do
+      Event.stub(:resolved).and_return([mock_model(Event, {})])
+      get :index
+      assigns(:resolved_events).count.should == 1
+    end
+
+    it "should set up an overall status" do
+      Status.stub(:overall_status).and_return("all_ok")
+      get :index
+      assigns(:overall_status).should == "all_ok"
+    end
+
+  end
+
+  context "GET show" do
+    it "should load successfully" do
+      Event.stub(:find).with("1").and_return(mock_model(Event, {}).as_null_object)
+      get :show, id: 1
+      assigns(:event).should_not be_nil
       response.should be_success
     end
   end
