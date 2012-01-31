55dd45b26301dbf2407bd7e31e4a78341f9c0652
Sean Handley <sean.handley@gmail.com>
2012-01-31 18:38:26 +0000

    Maded the main page into more of a dashboard to feed off to the other areas.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,13 @@
 class EventsController < ApplicationController
   def index
-    @active_events = Event.active.map {|e| EventDecorator.new(e)}
-    @scheduled_events = Event.scheduled.map {|e| EventDecorator.new(e)}
-    @resolved_events = Event.resolved.map {|e| EventDecorator.new(e)}
+    @active_events = Event.active.all(limit: 2).map {|e| EventDecorator.new(e)}
+    @scheduled_events = Event.scheduled.all(limit: 2).map {|e| EventDecorator.new(e)}
+    @resolved_events = Event.resolved.all(limit: 4).map {|e| EventDecorator.new(e)}
     @overall_status = Status.overall_status
   end
 
   def show
     @event = EventDecorator.new(Event.find(params[:id]))
   end
+
 end
